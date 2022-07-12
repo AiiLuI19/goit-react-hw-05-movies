7ec710165da6979c22ed446089c4888ecec5bd13
Iuliia <iuliiaiavorskaia@gmail.com>
2022-07-12 20:59:48 +0300

    add movies(no css)

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MovieSearchList from '../components/MovieSearchList';
 import SearchForm from '../components/SearchForm/SearchForm';
