1370c3309f38af6498e129e782516e07d73096df
Iuliia <iuliiaiavorskaia@gmail.com>
2022-07-12 21:48:50 +0300

    add lizzy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import components from './components';
-import pages from './pages';
-const { Home, Movies, PagesNonFound, MovieDetailsPage } = pages;
-const { Navigator, Reviews, Cast } = components;
+
+const Navigator = lazy(() =>
+  import('./components/Navigator' /* webpackChunkName: "navigator" */)
+);
+const Reviews = lazy(() =>
+  import('./components/Reviews' /* webpackChunkName: "reviews" */)
+);
+const Cast = lazy(() =>
+  import('./components/Cast' /* webpackChunkName: "cast" */)
+);
+const Home = lazy(() => import('./pages/Home' /* webpackChunkName: "home" */));
+const Movies = lazy(() =>
+  import('./pages/Movies' /* webpackChunkName: "movies" */)
+);
+const PagesNonFound = lazy(() =>
+  import('./pages/PagesNonFound.js' /* webpackChunkName: "pages-non-found" */)
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    './pages/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */
+  )
+);
 
 const App = () => {
   return (
     <main>
-      <Navigator />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<PagesNonFound />} />
-        </Routes>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navigator />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<PagesNonFound />} />
+          </Routes>
+        </div>
+      </Suspense>
     </main>
   );
 };
